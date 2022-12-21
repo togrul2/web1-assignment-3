de7a4aa5e5bd10936797e591aef1ba24bb1e452d
Nazrin Aliyeva <nazrinaliyeva0202@gmail.com>
2022-12-21 21:48:12 +0400

    added design for active navlink

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -6,9 +6,27 @@ export default function Header() {
     <header className={styles.header}>
       <nav className={styles.navigation}>
         <ul className={styles.list}>
-          <li><NavLink to='/' className={styles.link}>Home</NavLink></li>
-          <li><NavLink to='/movies' className={styles.link}>Movies</NavLink></li>
-          <li><NavLink to='/contact-me' className={styles.link}>Contact Me</NavLink></li>
+          <li>
+              <NavLink to='/' className={styles.link} style={({ isActive }) => {
+                  return isActive ? {color: '#f8f8f8', backgroundColor: '#6f76fc', borderRadius: '.4rem'} : {}
+              }}>
+                  Home
+              </NavLink>
+          </li>
+          <li>
+              <NavLink to='/movies' className={styles.link} style={({ isActive }) => {
+                  return isActive ? {color: '#f8f8f8', backgroundColor: '#6f76fc', borderRadius: '.4rem'} : {}
+              }}>
+                  Movies
+              </NavLink>
+          </li>
+          <li>
+              <NavLink to='/contact-me' className={styles.link} style={({ isActive }) => {
+                  return isActive ? {color: '#f8f8f8', backgroundColor: '#6f76fc', borderRadius: '.4rem'} : {}
+              }}>
+                  Contact Me
+              </NavLink>
+          </li>
         </ul>
       </nav>
     </header>
