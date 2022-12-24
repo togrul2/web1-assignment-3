0ef038c118319b8416a3f171c6720124932dd045
Nazrin Aliyeva <nazrinaliyeva0202@gmail.com>
2022-12-24 22:03:14 +0400

    added initial scratch for contact page form

--- src/pages/ContactMe/index.tsx
+++ src/pages/ContactMe/index.tsx
@@ -1,7 +1,33 @@
+import { FormControl } from '@mui/material';
+import { TextField } from '@mui/material';
+import { TextareaAutosize } from '@mui/material';
+import { Box } from '@mui/material';
+import { Button } from '@mui/material';
+
 export default function ContactMe() {
   document.title = "Contact me";
 
   return (
-    <>Contact me</>
+    <>
+      <FormControl>
+        <Box mb={1}>
+          <TextField id="outlined-basic" label="Subject" variant="outlined" />
+        </Box>
+
+        <Box mb={2}>
+          <TextField id="outlined-basic" label="Email address" variant="outlined" />
+        </Box>
+
+        <Box mb={1}>
+          <TextareaAutosize
+              aria-label="minimum height"
+              minRows={3}
+              placeholder=" Message"
+              style={{ width: 195 }}
+          />
+        </Box>
+        <Button variant="contained">Submit</Button>
+      </FormControl>
+    </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
 import path from "path";
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: [
