642a915f03675a8046dc75b7c4ab9abbd7625306
Nazrin Aliyeva <nazrinaliyeva0202@gmail.com>
2022-12-29 23:13:04 +0400

    designed contact form

--- src/pages/ContactMe/index.tsx
+++ src/pages/ContactMe/index.tsx
@@ -2,6 +2,7 @@ import { FormControl } from '@mui/material';
 import { TextField } from '@mui/material';
 import { TextareaAutosize } from '@mui/material';
 import { Box } from '@mui/material';
+import './ContactMe.scss';
 import { Button } from '@mui/material';
 
 export default function ContactMe() {
@@ -9,25 +10,31 @@ export default function ContactMe() {
 
   return (
     <>
-      <FormControl>
-        <Box mb={1}>
-          <TextField id="outlined-basic" label="Subject" variant="outlined" />
-        </Box>
+      {/*<FormControl>*/}
+      {/*  <Box mb={1}>*/}
+      {/*    <TextField id="outlined-basic" label="Subject" variant="outlined" />*/}
+      {/*  </Box>*/}
 
-        <Box mb={2}>
-          <TextField id="outlined-basic" label="Email address" variant="outlined" />
-        </Box>
+      {/*  <Box mb={2}>*/}
+      {/*    <TextField id="outlined-basic" label="Email address" variant="outlined" />*/}
+      {/*  </Box>*/}
 
-        <Box mb={1}>
-          <TextareaAutosize
-              aria-label="minimum height"
-              minRows={3}
-              placeholder=" Message"
-              style={{ width: 195 }}
-          />
-        </Box>
+      {/*  <Box mb={1}>*/}
+      {/*    <TextareaAutosize*/}
+      {/*        aria-label="minimum height"*/}
+      {/*        minRows={3}*/}
+      {/*        placeholder=" Message"*/}
+      {/*        style={{ width: 195 }}*/}
+      {/*    />*/}
+      {/*  </Box>*/}
+      {/*  <Button variant="contained">Submit</Button>*/}
+      {/*</FormControl>*/}
+      <form className="contact-form-container">
+        <input className="contact-details" placeholder="Subject" />
+        <input className="contact-details" placeholder="Email address" />
+        <textarea className="contact-details" placeholder="Your message" />
         <Button variant="contained">Submit</Button>
-      </FormControl>
+      </form>
     </>
   );
 }
