4da6694944c055243e88bf6cc6c8d7582d59d423
Nazrin Aliyeva <nazrinaliyeva0202@gmail.com>
2023-01-05 18:42:16 +0400

    designed not found page and added margin in movie details

--- src/components/Http404NotFound/index.tsx
+++ src/components/Http404NotFound/index.tsx
@@ -1,10 +1,13 @@
+import styles from './styles.module.scss';
+
 export default function() {
   document.title = "404 Not Found";
 
   return (
-    <>
-      <h1>Not found</h1>
-      <p>It seems like the page you are looking for does not exist</p>
-    </>
+    <div className={styles.notFoundPage}>
+      <h1 className={styles.error404}>4<span className={styles.span404}>0</span>4</h1>
+      <h4 className={styles.errorDescription}>Page Not Found</h4>
+      <p className={styles.errorText}>It seems like the page you are looking for does not exist</p>
+    </div>
   );
 }
